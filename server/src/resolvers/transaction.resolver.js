3d5495665dc74f4fb1650bc8f04b37ef38da42db
Naseem Khan <naseemsaffy@gmail.com>
2024-08-14 23:40:58 +0500

    feat: Update transaction resolver to fetch transactions for authenticated user

--- server/src/resolvers/transaction.resolver.js
+++ server/src/resolvers/transaction.resolver.js
@@ -1,9 +1,30 @@
-import { transactions } from "../data/index.js";
+import Transaction from "../models/transaction.model.js";
 
 const transactionResolver = {
   Query: {
-    transactions: () => {
-      return transactions;
+    transactions: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        if (!user) throw new Error("User not authenticated");
+
+        const transactions = await Transaction.find({ userId: user._id });
+        return transactions;
+      } catch (error) {
+        console.error("Error in fetching transactions: ", error);
+        throw new Error(error);
+      }
+    },
+
+    transaction: async (_, { id }) => {
+      try {
+        const transaction = await Transaction.findById(id);
+        if (!transaction) throw new Error("Transaction not found");
+
+        return transaction;
+      } catch (error) {
+        console.error("Error in fetching transaction by id: ", error);
+        throw new Error(error);
+      }
     },
   },
 

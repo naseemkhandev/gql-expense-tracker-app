c7ee39fe75ea057d3c8d33602b13fd558259f077
Naseem Khan <naseemsaffy@gmail.com>
2024-08-14 17:09:49 +0500

    Add user type definition to GraphQL schema

---
+++ server/src/typeDefs/user.typeDef.js
@@ -0,0 +1,38 @@
+const userTypeDef = `#graphql
+   type User {
+      id: ID!
+      name: String!
+      username: String!
+      password: String!
+      gender: String!
+      profilePicture: String
+   }
+
+   type Query {
+      users: [User!]
+      user(id: ID!): User
+      authUser: User
+   }
+
+   type Mutation {
+      register(input: RegisterInput!): User
+      login(input: LoginInput!): User
+      logout: LogoutResponse
+   }
+
+   input RegisterInput {
+      name: String!
+      username: String!
+      password: String!
+      gender: String!
+   }
+
+   input LoginInput {
+      username: String!
+      password: String!
+   }
+
+   type LogoutResponse {
+      message: String!
+   }
+`;

7d049492d48af2af7a5db07fafce204e9d88cfcf
Naseem Khan <naseemsaffy@gmail.com>
2024-08-14 20:14:05 +0500

    Refactor server index.js to use Apollo Server with Express

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
-import  mergedResolvers from "./src/resolvers/index.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import mergedResolvers from "./src/resolvers/index.js";
 import mergedTypeDefs from "./src/typeDefs/index.js";
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
   resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`🚀 Server ready at ${url}`);
+app.use(
+  "/",
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);

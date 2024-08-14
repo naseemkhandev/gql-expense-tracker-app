ad3d7511f6c2fa6128ae3c7d759d037e586d01f3
Naseem Khan <naseemsaffy@gmail.com>
2024-08-15 02:43:47 +0500

    feat: Update App.jsx to use Apollo Client for GraphQL API communication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Route, Routes } from "react-router-dom";
 
+import Header from "./components/header";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/notFound";
 import RegisterPage from "./pages/registerPage";
 import TransactionPage from "./pages/transactionPage";
-import NotFound from "./pages/notFound";
-import Header from "./components/header";
+import Loader from "./components/ui/loader";
+
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
 
 function App() {
   const authUser = true;
+  const { loading, error, data } = useQuery(GET_AUTHENTICATED_USER);
+
+  if (loading) return <Loader />;
+  if (error) return `Error! ${error.message}`;
+
+  console.log("Loading:", loading);
+  console.log("Error:", error);
+  console.log("Data:", data);
+
   return (
     <>
       {authUser && <Header />}

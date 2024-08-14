e69f51b188b00537bb3309c3996015a628ffa338
Naseem Khan <naseemsaffy@gmail.com>
2024-08-15 00:39:24 +0500

    feat: Add header component and update routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
-const App = () => {
-  return <div className="text-xl">App</div>;
-};
+import { Routes, Route } from "react-router-dom";
 
+import HomePage from "./pages/homePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/registerPage";
+import TransactionPage from "./pages/transactionPage";
+import NotFound from "./pages/notFound";
+import Header from "./components/header";
+
+function App() {
+  const authUser = true;
+  return (
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/transaction/:id" element={<TransactionPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
 export default App;
